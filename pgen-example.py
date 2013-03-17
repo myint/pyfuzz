7124240b356ee4b76847cc9e5a3c0eef55d0ae2b
Steven Myint <git@stevenmyint.com>
2013-03-17 13:47:52 -0700

    Make executable

--- pgen-example.py
+++ pgen-example.py
@@ -1,14 +1,17 @@
+#!/usr/bin/env python
+
 from __future__ import print_function
-from pgen import *
+
 import random
+
+from pgen import pgen_opts, ProgGenerator
+from pygen.cgen import CodeGenerator
 
 
 if __name__ == "__main__":
     pgen = ProgGenerator(pgen_opts, random.Random())
 
     m = pgen.generate()
-
-    from pygen.cgen import CodeGenerator
 
     cgen = CodeGenerator()
 
